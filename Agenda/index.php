f74aa86b6fd016ac5142b3c7b1ef58904514b8ad
hochiminh1996 <ambrsp@hotmail.com>
2023-12-12 18:35:17 -0300

    CRUD : AGENDA

---
+++ Agenda/index.php
@@ -0,0 +1,26 @@
+<?php 
+    include_once("config/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+    <!-- boostrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- font awesome : fonte de icones :olhinho, editar, deletar e etc () -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="<?=$BASE_URL?>css/style.css">
+    
+</head>
+<body>
+    <h1><?=$BASE_URL;?></h1>
+
+    <i class="fas fa-eye"></i>
+</body>
+</html>
