d3a787c5d2c00b8c3d11bfec06c959b6fdd821ea
hochiminh1996 <ambrsp@hotmail.com>
2023-12-15 17:21:44 -0300

    atualizando

--- Agenda/show.php
+++ Agenda/show.php
@@ -3,11 +3,13 @@
 
 ?>
     <div class="container" id="view-contact-container">
-        <h1 id="main-title"><?=$contact['name'];?></h1>
+        <h1 id="main-title-show"><?=$contact['name'];?></h1>
         <p class="bold">Telefone:</p>
         <p><?=$contact['phone'];?></p>
         <p class="bold">Observação:</p>
         <p><?=$contact['observations'];?></p>
+
+        <?php include_once("templates/btn.html");?>
     </div>
 
 <?php
